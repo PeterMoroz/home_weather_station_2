2325d0bed083d706cb71e270c7b96ac26627aebf
Peter Moroz <ptr.moroz@gmail.com>
2023-02-01 11:21:32 +0300

    add: LCD display

---
+++ workbench/lcd1602/lcd1602.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Weather Station");
+  lcd.setCursor(0, 1);  // col, row
+  lcd.print("  2nd edition  ");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
