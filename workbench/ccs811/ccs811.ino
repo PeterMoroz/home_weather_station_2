fc91a02c81d3e23e579150eec714d7e8d607b740
Peter Moroz <ptr.moroz@gmail.com>
2023-02-01 20:50:24 +0300

    add: CCS811 sensor

---
+++ workbench/ccs811/ccs811.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include "ccs811.h"
+
+CCS811 ccs811(-1, 0x5A);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Wire.begin();
+  delay(1000);
+  ccs811.set_i2cdelay(50);
+
+  if (!ccs811.begin()) {
+    Serial.println("setup: CCS811 init failed.");
+    return ;
+  }
+
+  if (!ccs811.start(CCS811_MODE_1SEC)) {
+    Serial.println("setup: CCS811 start failed.");
+    return;
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  uint16_t eco2, etvoc, errstat, raw;
+  ccs811.read(&eco2, &etvoc, &errstat, &raw);
+
+  if (errstat == CCS811_ERRSTAT_OK)
+  {
+    float co2 = eco2;
+    float tvoc = etvoc;
+    Serial.printf("loop: CO2 - %02.f ppm, TVOC - %02f ppb\n", co2, tvoc);
+  }
+  else if (errstat == CCS811_ERRSTAT_OK_NODATA)
+  {
+    Serial.println("loop: waiting for (new) data...");
+  }
+  else if (errstat & CCS811_ERRSTAT_I2CFAIL)
+  {
+    Serial.println("loop: I2C error");
+  }
+  else if (errstat & CCS811_ERRSTAT_ERRORS)
+  {
+    Serial.print("loop: error ");
+    Serial.print(errstat, HEX);
+    Serial.print(" ");
+    Serial.println(ccs811.errstat_str(errstat));
+  }
+
+  delay(1000);
+}
