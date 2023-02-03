386a2acd3c7cc149afe1e06f2964a07f7bb93238
Peter Moroz <ptr.moroz@gmail.com>
2023-02-03 21:52:04 +0300

    add: utility tool to parse output of serial client

---
+++ utils/data_parser/data_parser.py
@@ -0,0 +1,43 @@
+import re
+
+out_file_ccs811 = open('ccs811.csv', 'w')
+out_file_ccs811.write('timestamp;CO2;TVOC\n')
+
+
+def parse_ccs811(line, pos):
+    match = re.search(r'\d{4}/\d{2}/\d{2}T\d{2}:\d{2}:\d{2}', line)
+    if match != None:
+        group = match.group()
+        dt = group
+    else:
+        print(f'no date time in string {line}\n')
+        return
+    match = re.search(r'CO2 (\d+) ppm, TVOC (\d+.\d+) ppb', line[pos:])
+    if match != None:
+        co2 = match.group(1)
+        tvoc = match.group(2)
+    else:
+        print(f'no measurements data in string {line}\n')
+        return
+    # print(f'{dt};{co2};{tvoc}\n')
+    out_file_ccs811.write('{};{};{}\n'.format(dt, co2, tvoc))
+
+
+with open('output.txt', 'r') as in_file:
+    while True:
+        line = in_file.readline()
+        if not line:
+            break
+        pos = line.find('-')
+        if pos == -1:
+            continue
+        p0 = pos + 2
+        p1 = line.find(':', p0)
+        if p1 == -1:
+            continue
+        sensor = line[p0:p1]
+        # print(f'sensor:{sensor}\n')
+        if sensor == 'CCS811':
+            parse_ccs811(line, p1 + 1)
+        else:
+            print('unknown censor\n')
