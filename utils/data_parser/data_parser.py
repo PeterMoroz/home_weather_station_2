52cfa808e8382bfa0031daf98336ba24d7ed9d3d
Peter Moroz <ptr.moroz@gmail.com>
2023-02-12 21:30:21 +0300

    add: process readings from DHT22 sensor

--- utils/data_parser/data_parser.py
+++ utils/data_parser/data_parser.py
@@ -3,6 +3,11 @@ import re
 out_file_ccs811 = open('ccs811.csv', 'w')
 out_file_ccs811.write('timestamp;CO2;TVOC\n')
 
+out_file_dht22 = open('dht22.csv', 'w')
+out_file_dht22.write('timestamp;humidity;temperature\n')
+
+# 2023/02/08T08:28:38 - DHT22: humidity 20.20 %, temperature 22 celsius
+# 2023/02/08T08:28:38 - CCS811: CO2 400.00 ppm, TVOC 0.00 ppb    
 
 def parse_ccs811(line, pos):
     match = re.search(r'\d{4}/\d{2}/\d{2}T\d{2}:\d{2}:\d{2}', line)
@@ -12,7 +17,7 @@ def parse_ccs811(line, pos):
     else:
         print(f'no date time in string {line}\n')
         return
-    match = re.search(r'CO2 (\d+) ppm, TVOC (\d+.\d+) ppb', line[pos:])
+    match = re.search(r'CO2 (\d+.\d+) ppm, TVOC (\d+.\d+) ppb', line[pos:])
     if match != None:
         co2 = match.group(1)
         tvoc = match.group(2)
@@ -21,6 +26,24 @@ def parse_ccs811(line, pos):
         return
     # print(f'{dt};{co2};{tvoc}\n')
     out_file_ccs811.write('{};{};{}\n'.format(dt, co2, tvoc))
+        
+def parse_dht22(line, pos):
+    match = re.search(r'\d{4}/\d{2}/\d{2}T\d{2}:\d{2}:\d{2}', line)
+    if match != None:
+        group = match.group()
+        dt = group
+    else:
+        print(f'no date time in string {line}\n')
+        return
+    match = re.search(r'humidity (\d+.\d+) %, temperature (\d+) celsius', line[pos:])
+    if match != None:
+        h = match.group(1)
+        t = match.group(2)
+    else:
+        print(f'no measurements data in string {line}\n')
+        return
+    # print(f'{dt};{h};{t}\n')
+    out_file_dht22.write('{};{};{}\n'.format(dt, h, t))
 
 
 with open('output.txt', 'r') as in_file:
@@ -39,5 +62,7 @@ with open('output.txt', 'r') as in_file:
         # print(f'sensor:{sensor}\n')
         if sensor == 'CCS811':
             parse_ccs811(line, p1 + 1)
+        elif sensor == 'DHT22':
+            parse_dht22(line, p1 + 1)
         else:
             print('unknown censor\n')
--- utils/data_plotter/data_plotter.py
+++ utils/data_plotter/data_plotter.py
@@ -2,13 +2,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+import os.path
 
-df = pd.read_csv('ccs811.csv', sep=';', index_col='timestamp', parse_dates=['timestamp'])
 
-ax = df.plot(figsize=(15, 4))
-locator = mdates.MinuteLocator(interval=30)
-ax.xaxis.set_major_locator(locator)
-ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
-df.plot(subplots=True, figsize=(15, 6))
-plt.show()
+def draw_ccs811():
+    if not os.path.exists('ccs811.csv'):
+        return
 
+    df = pd.read_csv('ccs811.csv', sep=';', index_col='timestamp', parse_dates=['timestamp'])
+
+    ax = df.plot(figsize=(15, 4))
+    locator = mdates.MinuteLocator(interval=30)
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+    df.plot(subplots=True, figsize=(15, 6))
+    plt.show()
+
+def draw_dht22():
+    if not os.path.exists('dht22.csv'):
+        return
+        
+    df = pd.read_csv('dht22.csv', sep=';', index_col='timestamp', parse_dates=['timestamp'])
+
+    ax = df.plot(figsize=(15, 4))
+    locator = mdates.MinuteLocator(interval=30)
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(locator))
+    df.plot(subplots=True, figsize=(15, 6))
+    plt.show()
+
+
+if __name__ == '__main__':
+    draw_ccs811()
+    draw_dht22()
